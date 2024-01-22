1e77351c482ea78d79653c73164783b305ee0ffb
SaeedHabibi23 <ghulamsaeed54@gmail.com>
2024-01-22 21:41:11 +0430

    Vue Started Again

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
-  <select name="" id="" v-model="componentName">
-    <option value="Home">Home</option>
-    <option value="About">About</option>
-  </select>
-  <component :is="componentName">  </component>
+    <h1> Hello Vue JS </h1>
 </template>
 
 <script>
-import About from "./components/about.vue";
-import Home from "./components/Home.vue"
+
 export default {
 name: "App" , 
-components:{
-  About , 
-  Home
-} , 
+ 
 data(){
   return{
-    componentName: "Home"
+    
   }
 }
 }
