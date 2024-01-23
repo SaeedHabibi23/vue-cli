f82985ddd906cd8170145434ad4a6745d78d5083
SaeedHabibi23 <ghulamsaeed54@gmail.com>
2024-01-23 19:38:02 +0430

    Method worked

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
     <h1> {{ title }} </h1>
     <a :href="link"> Go To Vue Js </a>
+    <div>
+      {{ getText() }}
+    </div>
+
+    {{ getNotherText() }}
 </template>
 
 <script>
 
 export default {
-name: "App" , 
- 
-data(){
-  return{
-    title: 'This is the page Title',
-    link: 'https://vuejs.org'
+  name: "App",
+  data() {
+    return {
+      title: 'This is the page Title',
+      link: 'https://vuejs.org',
+      text1: 'Hello Vue',
+      text2: 'Hello React'
+    }
+  },
+  methods: {
+    getText() {
+      return this.text1;
+    },
+    getNotherText() {
+      return "React Course Learn";
+    }
   }
 }
-}
+
 </script>
 
